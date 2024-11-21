3e1588ebeee1db5aa60f1fa77e7e8ada3acb6667
Precious <155549089+PelvinDreams@users.noreply.github.com>
2024-11-21 21:48:42 +0000

    count message updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAddvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
 
   return (
@@ -14,6 +16,9 @@ function App() {
       <h1>{advice}</h1>
 
       <button onClick={getAddvice}>Get advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice
+      </p>
     </div>
   );
 }
